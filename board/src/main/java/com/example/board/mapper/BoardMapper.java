a3e4e8fbaf08fe56d04f1542fcc91608f1475ad6
yewon.seo <dodnjs1241@naver.com>
2022-04-16 22:53:16 +0900

    Add files via upload

--- board/src/main/java/com/example/board/mapper/BoardMapper.java
+++ board/src/main/java/com/example/board/mapper/BoardMapper.java
@@ -8,28 +8,25 @@ import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    //    게시글 목록
+//    게시글 목록
     public List<BoardVO> getList(Criteria criteria);
 
-    //    게시글 작성 (insert)
+//    게시글 작성
     public void insert(BoardVO boardVO);
 
-    //     게시글 작성
-//    selectKey를 통해서 전달된 결과는 boardVO필드에 매핑된 후 주입된다.
+//    게시글 작성
+//    selectKey를 통해서 전달된 결과는 boardVO 필드에 매핑된 후 주입된다.
     public void insertSelectBno(BoardVO boardVO);
 
-
-
-
-    //    게시글 수정(update)
+//    게시글 수정
     public int update(BoardVO boardVO);
 
-    //    게시글 삭제(delete)
+//    게시글 삭제
     public int delete(Long bno);
 
-    //    특정 게시글 가져오기(get)
+//    특정 게시글 가져오기
     public BoardVO get(Long bno);
 
-    //    전체 게시글 개수(
-    public int getTotal();
+//    전체 게시글 개수
+    public int getTotal(Criteria criteria);
 }
